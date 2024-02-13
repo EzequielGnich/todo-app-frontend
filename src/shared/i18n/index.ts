0e89ceacbe9626115eec15df5e67a2919df1143a
Ezequiel Gnich <ezequiel.gnich@syngenta.com>
2024-02-13 14:40:01 -0300

    feat: Change lib to dates

--- src/shared/i18n/index.ts
+++ src/shared/i18n/index.ts
@@ -1,7 +1,16 @@
+import {
+  Locale,
+  format as formatDate,
+  formatDistance,
+  formatRelative,
+  isDate,
+} from "date-fns";
+import { enUS, nl } from "date-fns/locale";
 import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import { DateTime } from "luxon";
 import Backend from "i18next-http-backend";
+import { initReactI18next } from "react-i18next";
+
+const locales = { en: enUS, nl } as Record<string, Locale>;
 
 i18n
   .use(Backend)
@@ -11,14 +20,23 @@ i18n
     fallbackLng: "en",
     interpolation: {
       format: (value, format, lng) => {
-        console.log(value, format, lng);
-        if (value instanceof Date) {
-          return DateTime.fromJSDate(value)
-            .setLocale(lng)
-            .toLocaleString(DateTime[format]);
+        if (isDate(value) && lng && lng in locales && format) {
+          const locale = locales[lng];
+
+          if (format === "short") return formatDate(value, "P", { locale });
+          if (format === "long") return formatDate(value, "PPPP", { locale });
+          if (format === "relative")
+            return formatRelative(value, new Date(), { locale });
+          if (format === "ago")
+            return formatDistance(value, new Date(), {
+              locale,
+              addSuffix: true,
+            });
+
+          return formatDate(value, format, { locale });
         }
+
         return value;
       },
-      escapeValue: false,
     },
   });
