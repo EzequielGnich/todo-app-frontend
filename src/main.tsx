ff80ca6616bc569225e0163bd865391e5772d4fc
Ezequiel Gnich <ezequiel.gnich@syngenta.com>
2024-02-13 11:53:25 -0300

    chore: Change file path to global scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./global.scss";
+
+import "./shared/global-styles/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
