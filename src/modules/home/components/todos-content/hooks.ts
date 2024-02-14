44c2aaa2ad32084411aaf39f602efeed642ad383
Ezequiel Gnich <ezequiel.gnich@syngenta.com>
2024-02-14 20:33:19 -0300

    refactor: Apply new way to render and get todos

--- src/modules/home/components/todos-content/hooks.ts
+++ src/modules/home/components/todos-content/hooks.ts
@@ -1,13 +1,26 @@
 import { useQuery } from "react-query";
 import { TEN_MINUTES } from "../../../../config/constants";
-import { GET_ALL_TODOS } from "./models";
+import { GET_ALL_TODOS, Todo, TodoStatus } from "./models";
 import { getAllTodos } from "./services";
+
+const splitTodosByStatus = (todos: Todo[]) => {
+  return todos.reduce((acc, todo) => {
+    const status = todo.status;
+    if (!acc[status]) {
+      acc[status] = [];
+    }
+    acc[status].push(todo);
+    return acc;
+  }, {} as Record<TodoStatus, Todo[]>);
+};
 
 export const useGetAllTodos = () => {
   return useQuery(
     GET_ALL_TODOS,
     async () => {
-      return getAllTodos();
+      const todos = await getAllTodos();
+
+      return splitTodosByStatus(todos);
     },
     {
       staleTime: TEN_MINUTES,
--- src/modules/home/components/todos-content/index.tsx
+++ src/modules/home/components/todos-content/index.tsx
@@ -1,38 +1,63 @@
-import { Button, Card, Col, Row } from "antd";
+import { Button, Card, Spin } from "antd";
 import Meta from "antd/es/card/Meta";
 import { useGetAllTodos } from "./hooks";
+import { Todo, TodoStatus } from "./models";
+
+const TodoSection: React.FC<{ status: string; todos: Todo[] }> = ({
+  status,
+  todos,
+}) => {
+  return (
+    <div key={status}>
+      <h2>{status}</h2>
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {todos.map((todo) => (
+          <Card
+            hoverable
+            style={{ width: 160, marginBottom: 16 }}
+            key={todo.id}
+          >
+            <span>{todo.status}</span>
+
+            <Meta title={todo.title} description={todo.content} />
+
+            <div style={{ marginTop: 10 }}>
+              <Button type="primary">Edit</Button>
+              <Button type="primary" danger>
+                Delete
+              </Button>
+            </div>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+};
+interface FetchProps<T> {
+  children: (data: T) => React.ReactNode;
+}
+
+const Fetch = <T,>({ children }: FetchProps<T>) => {
+  const { data, error, status, isLoading } = useGetAllTodos();
+
+  if (isLoading) return <Spin size="large" />;
+
+  if (status === "error") return <div>Error: {error?.message}</div>;
+
+  return <>{children(data)}</>;
+};
 
 export default function TodosContent() {
-  const { data } = useGetAllTodos();
-
   return (
-    <div
-      style={{
-        justifyContent: "center",
-        alignItems: "center",
-        display: "flex",
-        height: "100%",
-        width: "35%",
-      }}
-    >
-      <Row gutter={16}>
-        {data?.map((item) => (
-          <Col key={item.id} span={8}>
-            <Card hoverable style={{ width: 160, marginBottom: 16 }}>
-              {/* <span>{item.status}</span> */}
-
-              <Meta title={item.title} description={item.content} />
-
-              <div style={{ marginTop: 10 }}>
-                <Button type="primary">Edit</Button>
-                <Button type="primary" danger>
-                  Delete
-                </Button>
-              </div>
-            </Card>
-          </Col>
-        ))}
-      </Row>
+    <div>
+      <Fetch<Record<TodoStatus, Todo[]> | undefined>>
+        {(data) =>
+          data &&
+          Object.entries(data).map(([status, todos]) => (
+            <TodoSection status={status} todos={todos} />
+          ))
+        }
+      </Fetch>
     </div>
   );
 }
