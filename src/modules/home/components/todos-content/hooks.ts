fae0023aa6ebaa6bf31b1adf6390ebe61a096a49
Ezequiel Gnich <ezequiel.gnich@syngenta.com>
2024-02-17 13:14:36 -0300

    refactor: Change the position of sections of todos

--- src/modules/home/components/todos-content/hooks.ts
+++ src/modules/home/components/todos-content/hooks.ts
@@ -4,14 +4,18 @@ import { GET_ALL_TODOS, Todo, TodoStatus } from "./models";
 import { getAllTodos } from "./services";
 
 const splitTodosByStatus = (todos: Todo[]) => {
-  return todos.reduce((acc, todo) => {
-    const status = todo.status;
-    if (!acc[status]) {
-      acc[status] = [];
-    }
-    acc[status].push(todo);
-    return acc;
-  }, {} as Record<TodoStatus, Todo[]>);
+  return todos.reduce(
+    (acc, todo) => {
+      acc[todo.status].push(todo);
+
+      return acc;
+    },
+    {
+      [TodoStatus.ACTIVE]: [],
+      [TodoStatus.COMPLETED]: [],
+      [TodoStatus.DELETED]: [],
+    } as Record<TodoStatus, Todo[]>
+  );
 };
 
 export const useGetAllTodos = () => {
